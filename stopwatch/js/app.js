a8b9fb167acd09941516dfff4f88eef92adcf546
aussieyang <yang.j.li91@gmail.com>
2016-05-05 09:08:34 +1000

    wk2 ex4

---
+++ stopwatch/js/app.js
@@ -0,0 +1,7 @@
+console.log("Tic, Toc, Tic, Toc, Tic, Toc...");
+
+// STOPWATCH LOGIC GOES HERE!
+
+// As a user, I want a stopwatch to count timed passed second by second, so that I can track time passage.
+// As a user, I want to stop the stopwatch so that I can time specific events.
+// As a user, I want to reset the stopwatch so that I can start counting again from 0.
