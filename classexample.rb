3616f23877c50a99384836f71251c29a469c1251
aussieyang <yang.j.li91@gmail.com>
2016-05-20 09:36:54 +1000

    ruby class example

--- classexample.rb
+++ classexample.rb
@@ -1,6 +1,12 @@
 require 'pry'
 
+# Remember that a class and an instance of the class are different things.
+# The methods we write here can only be called on the instances.f
+# The initialize method is a special Ruby method that runs when .new() is called.
+
+# Class makes a factory pumping out Cat instances
 class Cat
+  # Every time Cat.new() is called, theses attributes are passed in
   def initialize (colour, age, evilness)
     @colour = colour
     @age = age
@@ -11,10 +17,10 @@ class Cat
     end
   end
 
+  # instancename.who will return this string
   def who
     p "This cat is #{@colour} and #{@age} years old and #{@evilness} evil."
   end
-
 end
 
-binding.pry
+# binding.pry
