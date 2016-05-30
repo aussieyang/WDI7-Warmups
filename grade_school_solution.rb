7eb77954bb2289be4befe83e6a4cd240befd88a4
aussieyang <yang.j.li91@gmail.com>
2016-05-30 10:23:51 +1000

    wk 6 ex1 sol

---
+++ grade_school_solution.rb
@@ -0,0 +1,36 @@
+# define class School
+class School
+  attr_reader :name, :database
+
+  def initialize(name)
+    # Instance variables
+    @name = name
+    @database = {}
+  end
+
+  def db
+    @database
+  end
+
+  def add(student, grade)
+    @database[grade] || = []
+    @database[grade] << name
+    #|| is a shorthand for :
+    # if @database[grade]          #if array exists
+        # database[grade] << name
+        # else
+        # @database[grade] = [name]
+  end
+
+  def grade(grade)
+    @database[grade]
+  end
+
+  def sort
+    @database.sort.inject({}) do |memo, elem|     #look into inject
+      memo[elem.first] = elem.last.sort
+      memo
+    end
+  end
+
+end
